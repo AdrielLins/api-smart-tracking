1356c4cac7e901f55dc60cbeed7bc4a1f109bde2
Adriel Lins Maciel <adriel.maciel@cwi.com.br>
2024-08-02 08:55:36 -0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { JogadoresModule } from './jogadores/jogadores.module';
+import { JogadoresService } from './jogadores/jogadores.service';
+import { JogadoresController } from './jogadores/jogadores.controller';
+
+@Module({
+  imports: [JogadoresModule],
+  controllers: [JogadoresController],
+  providers: [JogadoresService],
+})
+export class AppModule {}
